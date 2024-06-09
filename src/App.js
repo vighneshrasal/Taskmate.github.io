c5aa716a9e0b13aa5c75e21ab22d51cd96f39fa9
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-06-09 18:01:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { ShowTask } from './components/ShowTask';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const[tasklist, setTasklist] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [task, setTask] = useState({});
+  
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist))
+  }, [tasklist]);
+
+  return (
+    <div className="App">
+      <Header/>
+      <AddTask tasklist={tasklist} setTasklist={setTasklist} task={task} setTask={setTask}/>
+      <ShowTask tasklist={tasklist} setTasklist={setTasklist} task={task} setTask={setTask}/>
+    </div>
+  );
+}
+
+export default App;
